0cd2fea7f875ff3079538922e9bc4e75c5bd6a07
Shirley Wu <shirleyxueyangwu@gmail.com>
2016-09-04 16:03:01 -0700

    port block over to react

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import * as d3 from "d3";
+import _ from 'lodash';
+import data from './data/diving.json';
+import Canvas from './Canvas.js';
+
+module.exports = React.createClass({
+  getInitialState() {
+    return {
+      data: this.processData(data),
+    }
+  },
+
+  processData(data) {
+    var maxRadius = 45;
+    this.radiusScale = d3.scaleLinear().range([maxRadius / 3 * 2, maxRadius]);
+    this.yScale = d3.scaleLinear().range([0, 1]);
+
+    _.each(data, (d) => {
+      // create an artifical first score
+      d.breakdown.unshift(d.breakdown[0]);
+      d.processed = _.map(d.breakdown, function(score) {
+        return _.map(score[2], function(num) {return num * score[0]});
+      });
+    });
+
+    var difficulty = _.chain(data).map('breakdown').flatten().map(0).value();
+    var maxDiff = _.max(difficulty);
+    var scores = _.chain(data).map('processed').flattenDeep().value();
+    var minY = _.min(scores);
+    var maxY = _.max(scores);
+
+    this.radiusScale.domain([1, maxDiff]);
+    this.yScale.domain([minY, maxY]);
+
+    return data;
+  },
+
+  render() {
+    var women = _.filter(this.state.data, (d) => d.gender === 'W');
+    var men = _.filter(this.state.data, (d) => d.gender === 'M');
+    var scales = {radiusScale: this.radiusScale, yScale: this.yScale};
+
+    return (
+      <div>
+        <Canvas data={women} scales={scales} />
+        <Canvas data={men} scales={scales} />
+      </div>
+    );
+  }
+});
---
+++ src/Canvas.js
@@ -0,0 +1,241 @@
+import React from 'react';
+import * as d3 from "d3";
+import _ from 'lodash';
+
+// properties
+var maxRadius = 45;
+var padding = 75;
+var width = 900;
+var height = 900;
+var TWO_PI = 2 * Math.PI;
+var colors = {
+  'China': [[255,0,0], [255,255,0]],
+  'United States': [[187, 19, 62], [0, 44, 119]],
+  'Great Britain': [[0, 39, 118], [198, 12, 48]],
+  'Italy': [[0, 146, 70], [206, 43, 55]],
+  'Australia': [[0, 0, 139], [255, 0, 0]],
+  'Malaysia': [[1, 0, 102], [255, 204, 0]],
+  'Canada': [ [255, 255, 255], [255, 0, 0]],
+  'F': [250,176,189],
+  'M': [187,209,222]
+};
+
+// generate the data for just one of the circles in the flow line
+// majority of this function is taken from
+// Dan Gries's tutorial http://rectangleworld.com/blog/archives/462
+// in particular the function setLinePoints
+function generateCircleData(scores, yScale) {
+  var circle = {
+    first: {x: 0, y: 1}
+  };
+  var last = {x: 1, y: 1};
+  var minY = 1;
+  var maxY = 1;
+  var point, nextPoint;
+  var dx, newX, newY;
+
+  // connect first point with the last
+  circle.first.next = last;
+  _.each(scores, (score) => {
+    point = circle.first;
+    while (point.next) {
+      nextPoint = point.next;
+      dx = nextPoint.x - point.x;
+      newX = 0.5 * (point.x + nextPoint.x);
+      newY = 0.5 * (point.y + nextPoint.y);
+      // vary the y-pos by the score, but subtract it
+      // by what is around the mid-point so that
+      // some are positive and others are negative
+      newY += dx * (yScale(score) * 2 - 1);
+
+      var newPoint = {x: newX, y: newY};
+
+      //min, max
+      if (newY < minY) {
+        minY = newY;
+      }
+      else if (newY > maxY) {
+        maxY = newY;
+      }
+
+      // insert mid-point
+      newPoint.next = nextPoint;
+      point.next = newPoint;
+
+      point = nextPoint;
+    }
+  })
+
+  // normalize to values between 0 and 1
+  if (maxY !== minY) {
+    var normalizeRate = 1/(maxY - minY);
+    point = circle.first;
+    while (point != null) {
+      point.y = normalizeRate*(point.y - minY);
+      point = point.next;
+    }
+  }
+
+  return circle;
+}
+
+function tweenPoints(circle1, circle2) {
+  // interpolate all the points of the circles
+  var interpolators = _.map(circle1, function(point1, i) {
+    return {
+      x: d3.interpolate(point1.x, circle2[i].x),
+      y: d3.interpolate(point1.y, circle2[i].y)
+    };
+  });
+  return function(t) {
+    return _.map(interpolators, function(interpolate) {
+      return {x: interpolate.x(t), y: interpolate.y(t)};
+    });
+  };
+}
+
+module.exports = React.createClass({
+  getInitialState() {
+    return {
+      flows: [],
+    }
+  },
+
+  componentDidMount() {
+    this.refs.canvas.width = width;
+    this.refs.canvas.height = height;
+    this.ctx = this.refs.canvas.getContext('2d');
+    this.ctx.globalCompositeOperation = 'overlay';
+
+    var flows = this.generateFlowData(this.props.data);
+    this.calculateCircles(flows);
+    this.drawFlows(flows);
+    this.setState({flows});
+  },
+
+  // generate the flow line, given one event for one team
+  generateFlowData(teams) {
+    var xWidth = (width - 2 * padding) / (teams.length - 1);
+    return _.map(teams, (team, i) => {
+      var gradient = this.ctx.createRadialGradient(0, 0, 0, 0, 0, maxRadius);
+      gradient.addColorStop(1, 'rgba(' + colors[team.country][0] + ',0.1)');
+      gradient.addColorStop(0, 'rgba(' + colors[team.country][1] + ',0.1)');
+
+      return {
+        centerX: xWidth * i + padding,
+        centerY: padding,
+        stroke: gradient,
+        fill: 'rgba(' + colors[team.gender] + ', 0.01)',
+        radii: _.map(team.breakdown, (scores) => {
+          return this.props.scales.radiusScale(scores[0]);
+        }),
+        points: _.map(team.processed, (scores) => {
+          return generateCircleData(scores, this.props.scales.yScale);
+        }),
+        length: _.map(team.breakdown, (scores) => {
+          return Math.floor(scores[1]) * 4;
+        }),
+        rotations: _.map(team.breakdown, (scores) => {
+          return scores[1] / team.total;
+        }),
+        dataPoints: [],
+        totalLength: Math.floor(team.total) * 4,
+        elapsed: 0,
+        data: team
+      }
+    });
+  },
+
+  // given set of points making up a squiggly line
+  // turn it into a squiggly imperfect circle
+  // also calculate the interpolators for them
+  calculateCircles(flows) {
+    _.each(flows, (flow) => {
+      flow.circles = [];
+      flow.interpolators = [];
+      var prevCircle = null;
+      _.each(flow.points, function(points, i) {
+        // calculate circles
+        var point = points.first;
+        var rotation = flow.rotations[i];
+        var radii = flow.radii[i];
+        var circle = [];
+
+        var theta = TWO_PI * (point.x + rotation);
+        var radius = radii * point.y;
+        var x = radius * Math.cos(theta);
+        var y = radius * Math.sin(theta);
+        circle.push({x: x, y: y});
+
+        while (point.next) {
+          point = point.next;
+
+          // given its x and y, calculate its theta and radius
+          theta = TWO_PI * (point.x + rotation);
+          radius = radii * point.y;
+
+          x = radius * Math.cos(theta);
+          y = radius * Math.sin(theta);
+
+          circle.push({x: x, y: y});
+        }
+        flow.circles.push(circle);
+
+        // now calculate the interpolators
+        if (prevCircle) {
+          var interpolators = tweenPoints(prevCircle, circle);
+          flow.interpolators.push(interpolators);
+        }
+
+        prevCircle = circle;
+      });
+    });
+  },
+
+  drawFlows(flows) {
+    _.each(flows, (flow) => {
+      var drawCount = 0;
+
+      _.each(flow.interpolators, (interpolator, i) => {
+        var length = flow.length[i + 1];
+        this.ctx.strokeStyle = flow.stroke;
+
+        // for each of the interpolators, draw the circle length amount of times
+        _.times(length, (i) => {
+          this.ctx.beginPath();
+          drawCount += 1;
+
+          flow.centerY += 0.5;
+          var xOffset = (_.last(flow.radii) / 3) *
+            Math.sin(drawCount/flow.data.total * TWO_PI);
+          this.ctx.setTransform(1, 0, 0, 1, flow.centerX + xOffset, flow.centerY);
+
+          var points = interpolator(i / length);
+          _.each(points, (pos) => {
+            this.ctx.lineTo(pos.x, pos.y);
+          });
+
+          this.ctx.closePath();
+          this.ctx.stroke();
+        });
+
+        // if (t === length) {
+        //   var x1 = flow.centerX + xOffset;
+        //   var maxX = _.maxBy(flow.circles[i + 1], 'x').x;
+        //   flow.dataPoints.push({
+        //     x1: x1,
+        //     x2: x1 + maxX,
+        //     y: flow.centerY,
+        //     score: flow.data.breakdown[i + 1]
+        //   });
+        // }
+      });
+    });
+  },
+
+  render() {
+    return (
+      <canvas ref='canvas' />
+    );
+  }
+});
